6957f0505d97b7b6a482bbefe57dc935b54369fa
numhebzaba <num.ptp@gmail.com>
2022-11-17 23:19:01 +0700

    Add :  Pic Add : regis and login in index.py

---
+++ index.py
@@ -0,0 +1,119 @@
+from flask import Flask, render_template,url_for,redirect,request,session
+#Create mysql connector to server database
+import mysql.connector as mysql
+# File and Directory
+import os
+# Hash Function
+import hashlib
+import random
+import datetime
+
+conn = mysql.connect(
+    host = 'localhost',
+    user = 'root',
+    passwd = 'n.0818570429',
+    port = 3306,
+    database = 'project_mdt'
+)
+
+app = Flask(__name__)
+template_folder = os.path.join(os.path.dirname(__file__), "templates/")
+app.static_folder = 'static'
+app.static_url_path = '/static'
+
+app.secret_key = "Web Project" #can name anything
+
+#size not ocer 2MB
+app.config['MAX_CONTENT_LENGTH'] = 2 * 1024 * 1024 
+
+#picture folder
+UPLOAD_PICTURE = os.path.join(os.path.dirname(__file__), "static\picture")
+
+@app.route('/', methods=["GET","POST"])
+def index():
+    session['user'] = ''
+    session['audit'] = False
+    return render_template('login.html')
+
+@app.route('/register', methods=["GET","POST"])
+def register():
+    return render_template('register.html')
+
+@app.route('/validate-register', methods=["GET","POST"])
+def validate_register():
+    email = request.form['email']
+    user = request.form['user']
+    password = request.form['password']
+    #cfpassword = request.form['cfpassword']
+
+    if email !='' and user !='':
+        encypt_passwd = hashlib.md5(password.encode()).hexdigest()
+        conn.reconnect()
+        cur = conn.cursor()
+        sql_createTable = '''
+            CREATE TABLE IF NOT EXISTS username (id INT AUTO_INCREMENT PRIMARY KEY, username VARCHAR(255), email VARCHAR(100),password VARCHAR(100), audit TINYINT,)
+        '''
+        #val = (user, encypt_passwd, fname, 1) #tuple
+        cur.execute(sql_createTable)
+
+        conn.reconnect()
+        cur = conn.cursor()
+        sql_insert = '''
+            INSERT INTO username(username,email,password,audit)
+            VALUES(%s, %s, %s, %s)
+        '''
+        val = (user, email, encypt_passwd, 1) #tuple
+        cur.execute(sql_insert, val)
+        conn.commit()
+        conn.close()
+        
+        return render_template('login.html')
+    else:
+        return redirect('/register')
+
+@app.route('/login', methods=["GET","POST"])
+def login():
+    return redirect('/')
+
+@app.route('/validate-login', methods=["GET", "POST"])
+def validate_login():
+    user = request.form['user']
+    password = request.form['password']
+    try:
+        if user !="" and password !="":
+            conn.reconnect()
+            cur = conn.cursor()
+            sql = '''
+                SELECT password FROM username
+                WHERE username=%s and audit=1
+            '''
+            val = (user,) #tuple
+            cur.execute(sql, val)
+            data = cur.fetchone() #1 record ('werwerwerwerwerw','dadasdasdasd') <- dat[0]
+            conn.close()
+
+            encrpt_password = hashlib.md5(password.encode()).hexdigest()
+            if encrpt_password == data[0]:
+                session['user'] = user
+                session['audit'] = True    
+                return render_template('index.html')
+            else:
+                return redirect('/login')
+
+        else:
+            return redirect('/login')
+    except:
+        return redirect('/login')
+
+@app.route('/logout', methods=["GET","POST"])
+def sign_out():
+    session.pop('user', None)
+    session.pop('audit', None)
+    return redirect('/')
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0',port=5000,debug=True)
